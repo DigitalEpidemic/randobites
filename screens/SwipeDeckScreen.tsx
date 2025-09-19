3178df5d314992b0ed4cdfd90b34ab7a9ec39ecd
Jeffrey Polasz <jeff_polasz@hotmail.com>
2025-09-19 17:22:08 -0400

    Horizontally center No more restaurants placeholder

--- screens/SwipeDeckScreen.tsx
+++ screens/SwipeDeckScreen.tsx
@@ -223,6 +223,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     padding: 40,
+    alignSelf: "center",
   },
   noMoreCardsText: {
     fontSize: 24,
